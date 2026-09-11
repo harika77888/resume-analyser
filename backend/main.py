a2e388ac384d6b8bbf40cc26b1158f0d13274f8c
harika77888 <harikakudupudi1@gmail.com>
2026-09-11 11:47:05 +0530

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, UploadFile, File
+import pdfplumber
+from fastapi.middleware.cors import CORSMiddleware
+from skills_data import job_roles
+ 
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+ 
+ 
+@app.get("/")
+def home():
+    return {"message": "Resume Analyser backend is working!"}
+ 
+ 
+@app.post("/upload")
+async def upload_resume(file: UploadFile = File(...)):
+    text = ""
+    with pdfplumber.open(file.file) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text.lower()
+ 
+    found_skills = []
+    for role, skills in job_roles.items():
+        for skill in skills:
+            if skill in text and skill not in found_skills:
+                found_skills.append(skill)
+ 
+    role_matches = []
+    for role, required_skills in job_roles.items():
+        matched = [s for s in required_skills if s in found_skills]
+        if matched:
+            match_percent = round((len(matched) / len(required_skills)) * 100)
+            role_matches.append({
+                "role": role,
+                "matched_skills": matched,
+                "match_percent": match_percent
+            })
+ 
+    role_matches.sort(key=lambda x: x["match_percent"], reverse=True)
+ 
+    return {
+        "skills_found": found_skills,
+        "recommended_jobs": role_matches
+    }
+ 
---
+++ backend/skills_data.py
@@ -0,0 +1,12 @@
+job_roles={
+      "Data Analyst": ["python","sql","excel","power bi","tableau"],
+       "web Developer": ["html","css","javascript","react","node"],
+       "ML Engineer": ["python","machine learning","tenserflow","pandas","numpy"],
+    "Data Scientist": ["python", "machine learning", "pandas", "numpy", "sql", "deep learning"],
+    "AI/ML Engineer": ["python", "tensorflow", "pytorch", "machine learning", "deep learning", "nlp"],
+    "Prompt Engineer": ["prompt engineering", "llm", "python", "nlp", "openai api"],
+    "ServiceNow Developer": ["servicenow", "javascript", "itsm", "workflow automation", "glide"],
+    "Frontend Developer": ["html", "css", "javascript", "react", "responsive design"],
+    "Backend Developer": ["python", "node.js", "django", "flask", "sql", "api development"],
+    "Full Stack Developer": ["html", "css", "javascript", "react", "node.js", "python", "sql"],
+}
